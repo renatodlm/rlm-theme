71b9659e3a969dc49b022799dbe4ba37d87f39ad
Renato Marques <renato@buildbox.com.br>
2024-11-10 13:43:59 -0300

    🚚

--- rlmwp/wp-content/themes/rlm-theme/header.php
+++ rlmwp/wp-content/themes/rlm-theme/header.php
@@ -35,17 +35,17 @@
          }
 
          ?>
+         <nav id="site-navigation" class="main-navigation">
+            <button class="menu-toggle" aria-controls="primary-menu"
+               aria-expanded="false"><?php esc_html_e('Primary Menu', 'rlm_theme'); ?></button>
+            <?php
+            wp_nav_menu(
+               array(
+                  'theme_location' => 'menu-1',
+                  'menu_id'        => 'primary-menu',
+               )
+            );
+            ?>
+         </nav>
       </div>
-      <nav id="site-navigation" class="main-navigation">
-         <button class="menu-toggle" aria-controls="primary-menu"
-            aria-expanded="false"><?php esc_html_e('Primary Menu', 'rlm_theme'); ?></button>
-         <?php
-         wp_nav_menu(
-            array(
-               'theme_location' => 'menu-1',
-               'menu_id'        => 'primary-menu',
-            )
-         );
-         ?>
-      </nav>
    </header>
