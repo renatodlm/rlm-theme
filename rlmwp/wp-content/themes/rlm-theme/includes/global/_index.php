d8199d89a011d5a52bbf9ccbb97b87cb0f5e7fc3
Renato Marques <renato@buildbox.com.br>
2024-11-10 13:43:01 -0300

    🚑

--- rlmwp/wp-content/themes/rlm-theme/includes/global/_index.php
+++ rlmwp/wp-content/themes/rlm-theme/includes/global/_index.php
@@ -11,5 +11,5 @@ require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'handle-assets.php']);
 require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'PHPMailer.php']);
 require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'svg-support.php']);
 require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'dns-prefetch.php']);
-require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'template-tags']);
-require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'jetpack']);
+require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'template-tags.php']);
+require_once implode(DIRECTORY_SEPARATOR, [__DIR__, 'jetpack.php']);
